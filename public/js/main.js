991f799f55b38f4fb3e2ffbfb32c19d8e172c497
Maxence <lemairemaxence@hotmail.com>
2020-12-28 12:40:04 +0100

    exo ok

---
+++ public/js/main.js
@@ -0,0 +1,39 @@
+// 1
+
+let tartine = "J'aime les tartines et j'en mange"
+console.log(tartine);
+
+let quatre = "4 par jour"
+console.log(quatre);
+
+console.log(tartine + " " + quatre);
+
+let troisieme = 5
+let quatrieme = 35
+
+let resultat = troisieme + quatrieme
+console.log(resultat);
+
+console.log(tartine + " " + troisieme);
+
+// 2
+
+let salut = "Salut tout le monde"
+console.log(salut);
+let aurevoir = "Au revoir"
+console.log(aurevoir);
+
+salut = aurevoir
+console.log(salut);
+
+// 3
+
+let quatrechiffre = 4
+let sixlettres = "6"
+console.log(quatrechiffre > sixlettres);
+console.log(quatrechiffre < sixlettres);
+console.log(quatrechiffre == sixlettres);
+console.log(quatrechiffre != sixlettres);
+console.log(quatrechiffre === sixlettres);
+console.log(quatrechiffre !== sixlettres);
+
